ebd75a1d15bf7083e12fda93b24cfaaff6e149e6
Luis Valero <lluisvalerodelai@gmail.com>
2025-10-14 21:23:08 +0200

    logs

--- tasks/tasks/__init__.py
+++ tasks/tasks/__init__.py
@@ -6,5 +6,8 @@ WARMUP_SIZE = 512
 WARMUP_SIZE2 = 2000
 
 # Precompile the function for float64 arrays
+print("compiling matrix...")
 matmul_task(WARMUP_SIZE)
+print("50% there...")
 matmul_task(WARMUP_SIZE2)
+print("finished compiling")
